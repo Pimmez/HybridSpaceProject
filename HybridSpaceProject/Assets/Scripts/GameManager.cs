9eab8ab7452f05d4f775f7b96696639affcda38b
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-12-16 14:56:52 +0100

    -Testing FirstSetup -Imported Text meh pro for better graphics in UI

---
+++ HybridSpaceProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private List<GameObject> imageObjects = new List<GameObject>();
+
+	public void StartSomething(Image _image)
+	{
+		for (int i = 0; i < imageObjects.Count; i++)
+		{
+			imageObjects[0].SetActive(true);
+		}
+	}
+
+	public void EMOTE1(GameObject _emote1)
+	{
+
+	}
+}
