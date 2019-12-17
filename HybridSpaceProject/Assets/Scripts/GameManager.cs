ceca2370d8840b99b06e6bd093f6b4b15f325fbc
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-12-18 00:43:31 +0100

    Significant Update: -Added Stranger(Douwe) animations -Updated all the friend(jewelle) animations to Image.Sprite -Updated User Interface -Changed all the images from Default to Sprite (UI) -Updated PimScene.unity -Added Folder with stranger sprites -Updated Desktop.controller (Animation Controller) -Updated GameManager.cs

--- HybridSpaceProject/Assets/Scripts/GameManager.cs
+++ HybridSpaceProject/Assets/Scripts/GameManager.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] private Image image;
+	[SerializeField] private Animator anim;
 
 	public void ChangeSprite(Sprite _UpdateSprite)
 	{
 		image.sprite = _UpdateSprite;
+	}
+
+	public void ChangeSpriteWithAnimation(string _stateName)
+	{
+		anim.Play(_stateName);
 	}
 }
