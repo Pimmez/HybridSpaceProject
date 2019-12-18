013a1579edc99301ab2282246f14d6815968bf4e
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-12-18 10:58:22 +0100

    -Changed Project settings and code to make the 2 screen work

--- HybridSpaceProject/Assets/Scripts/DisplayActivation.cs
+++ HybridSpaceProject/Assets/Scripts/DisplayActivation.cs
@@ -8,19 +8,19 @@ public class DisplayActivation : MonoBehaviour
 	{
 		for (int i = 0; i < Display.displays.Length; i++)
 		{
-			Display.displays[i].Activate(1024, 720, 60);
+			Display.displays[i].Activate(1920, 1080, 60);
 		}
 
 		if (Display.displays.Length > 1)
 		{
 			//display 0 is set by default
 			Display.displays[1].Activate();
-			Display.displays[1].SetParams(1024, 720, 0, 0);
+			Display.displays[1].SetParams(1920, 1080, 0, 0);
 		}
 		if (Display.displays.Length > 2)
 		{
 			Display.displays[2].Activate();
-			Display.displays[2].SetParams(1024, 720, 0, 0);
+			Display.displays[2].SetParams(1920, 1080, 0, 0);
 		}
 
 	}
