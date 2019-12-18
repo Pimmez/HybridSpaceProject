736fe85c9bd12bfc43c38e93eb8b54c880ad72bb
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-12-18 03:26:22 +0100

    -Added a lot of animations, everything is working just fine
    -BUILDING DOESN"T WORK WITH 2 SCREENS AAAAAHHHHHHHHHHHHHHHH

---
+++ HybridSpaceProject/Assets/Scripts/DisplayActivation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayActivation : MonoBehaviour
+{
+	private void Awake()
+	{
+		for (int i = 0; i < Display.displays.Length; i++)
+		{
+			Display.displays[i].Activate(1024, 720, 60);
+		}
+
+		if (Display.displays.Length > 1)
+		{
+			//display 0 is set by default
+			Display.displays[1].Activate();
+			Display.displays[1].SetParams(1024, 720, 0, 0);
+		}
+		if (Display.displays.Length > 2)
+		{
+			Display.displays[2].Activate();
+			Display.displays[2].SetParams(1024, 720, 0, 0);
+		}
+
+	}
+}
